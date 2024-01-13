956c3df40c396519c04cec9c0b515795d1d22f30
liruyi <liruyi0229@outlook.com>
2024-01-13 23:18:33 +0800

    修改脚本

--- src/page_object/baidu/sitepage.py
+++ src/page_object/baidu/sitepage.py
@@ -6,7 +6,7 @@ from common.play_wright.sync_playwright_base import SyncPlayWrightWrapper
 class main_page_element(SyncPlayWrightWrapper):
 
     def serach_input(self):
-        self.get_locator('#kw').type("beads")
+        self.get_locator('#kw').type("playwright定位")
 
     def click_search(self):
         self.get_locator('#su').click()
