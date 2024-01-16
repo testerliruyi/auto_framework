b75173881b02b4fff3bce3ec436701a2b44a8d51
liruyi <liruyi0229@outlook.com>
2024-01-16 05:32:29 +0000

    update config/setting.py.
    Signed-off-by: liruyi <liruyi0229@outlook.com>

--- config/setting.py
+++ config/setting.py
@@ -1,17 +1,43 @@
+"""
+配置文件，项目常用配置信息
+"""
 import os
-# 配置信息
-class Setting:
+from common.file_handle.get_file_list import get_files
+
+
+class ConfigInfo:
     # 项目根本目录
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+    # 配置环境
+    ENV = "YD02"
+
+    # yaml文件测试案例保存目录
+    TEST_CASE_FILE = os.path.join(BASE_DIR, "test_manage/test_case_file")
+
+    # py测试可执行案例保存目录
+    TEST_CASE_PATH = os.path.join(BASE_DIR, "test_manage/test_case")
+
+    # test_data文件目录
+    TEST_DATA_PATH = os.path.join(BASE_DIR, "test_manage/test_data")
+
+    # 不同环境测试使用链接配置文件
+    ENV_CONFIG_FILE = os.path.join(BASE_DIR, "config/env_config.ini")
+
+    # 数据库信息保存文件
+    DATABASE_INFO_FILE = os.path.join(BASE_DIR, "config/database_info.yaml")
+
     # 视频存放路径
-    videoPath = r"E:\视频目录\12.25压缩包"
-    # 视频号url
-    url = "https://channels.weixin.qq.com/login.html"
+    videoPath = r"E:\视频目录\YYW.AI-1.15"
 
     # 循环次数
     loop_count = 5
 
+    # ffmpeg 安装地址
+    ffmpeg_DIR = r"D:\ffmpeg-6.1.1\bin"
+
 
 if __name__ == "__main__":
-    print(ConfigInfo.BASE_DIR)
+    # 获取文件列表
+    for i in get_files(ConfigInfo.videoPath):
+        print(i)
