aeade90b5b5f5aa26cb248d58034bd6ded0d97b8
liruyi <liruyi0229@outlook.com>
2024-01-18 12:42:02 +0000

    增加日志对象初始化脚本
    Signed-off-by: liruyi <liruyi0229@outlook.com>

---
+++ common/log/set_logger.py
@@ -0,0 +1,48 @@
+"""
+func:设置log格式，返回日志对象
+"""
+import logging
+import traceback
+from config.setting import ConfigInfo
+
+
+class SetLogger(ConfigInfo):
+
+    def __init__(self):
+        self.file = ConfigInfo.LOG_FILE_PATH
+        self.log = logging.getLogger(__name__)
+        self.log.setLevel(logging.INFO)
+
+    @staticmethod
+    def set_formatter():
+        formatter_1 = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s")
+        return formatter_1
+
+    # 日志文件设置
+    def set_file_handler(self):
+        file_handler = logging.FileHandler(filename=self.file, mode='w', encoding='utf-8')
+        file_handler.setFormatter(self.set_formatter())
+        file_handler.setLevel(logging.INFO)
+        self.log.addHandler(file_handler)
+
+    def set_Stream_handler(self):
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(self.set_formatter())
+        stream_handler.setLevel(logging.INFO)
+        self.log.addHandler(stream_handler)
+
+    def get_logger(self):
+        self.set_file_handler()
+        return self.log
+
+
+def logger_obj():
+    logger = SetLogger().get_logger()
+    return logger
+
+
+if __name__ == "__main__":
+    logger_obj()
+
+
+
