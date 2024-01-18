1a454583c916e303f26d8146fb4ecc3db8694c0e
liruyi <liruyi0229@outlook.com>
2024-01-18 12:39:26 +0000

    update config/setting.py.
    Signed-off-by: liruyi <liruyi0229@outlook.com>

--- config/setting.py
+++ config/setting.py
@@ -2,7 +2,10 @@
 配置文件，项目常用配置信息
 """
 import os
+import pathlib
+
 from common.file_handle.get_file_list import get_files
+from common.params import Params
 
 
 class ConfigInfo:
@@ -16,7 +19,7 @@ class ConfigInfo:
     TEST_CASE_FILE = os.path.join(BASE_DIR, "test_manage/test_case_file")
 
     # py测试可执行案例保存目录
-    TEST_CASE_PATH = os.path.join(BASE_DIR, "test_manage/test_case")
+    TEST_CASE_PATH = os.path.join(BASE_DIR, "test_manage/api_test_case")
 
     # test_data文件目录
     TEST_DATA_PATH = os.path.join(BASE_DIR, "test_manage/test_data")
@@ -27,8 +30,11 @@ class ConfigInfo:
     # 数据库信息保存文件
     DATABASE_INFO_FILE = os.path.join(BASE_DIR, "config/database_info.yaml")
 
+    # 日志保存文件路径
+    LOG_FILE_PATH = os.path.join(BASE_DIR, f"log\\test_{Params.date_formatter(formatter='%Y%m%d')}.log")
+
     # 视频存放路径
-    videoPath = r"E:\视频目录\YYW.AI-1.15"
+    videoPath = "E:\视频目录\YYW.AI-1.18"
 
     # 循环次数
     loop_count = 5
@@ -37,7 +43,9 @@ class ConfigInfo:
     ffmpeg_DIR = r"D:\ffmpeg-6.1.1\bin"
 
 
+
 if __name__ == "__main__":
     # 获取文件列表
-    for i in get_files(ConfigInfo.videoPath):
-        print(i)
+    # for i in get_files(ConfigInfo.videoPath):
+    #     print(i)
+    print(ConfigInfo.LOG_FILE_PATH)
