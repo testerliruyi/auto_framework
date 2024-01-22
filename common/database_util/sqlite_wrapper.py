1f824e4d3203742a76498229a1ee4d6919fd8fd2
liruyi <liruyi0229@outlook.com>
2024-01-22 11:17:51 +0000

    update common/database_util/sqlite_wrapper.py.
    Signed-off-by: liruyi <liruyi0229@outlook.com>

--- common/database_util/sqlite_wrapper.py
+++ common/database_util/sqlite_wrapper.py
@@ -1,31 +1,48 @@
 import sqlite3
 
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 class SqliteOpera:
     def __init__(self):
-        self.Conn = sqlite3.connect('../util_scripts/myTest.sqlite')
-        self.Cursor = self.Conn.cursor()
-
-    def exe_query(self, sql):
         try:
-            self.Cursor.execute(sql)
+            self.conn = sqlite3.connect('../util_scripts/myTest.sqlite')
+
+        except Exception as err:
+            print("数据库连接有误", err)
+        else:
+            self.conn.row_factory = dict_factory
+            self.cursor = self.conn.cursor()
+
+    def exe_query(self, sql, *args):
+        try:
+            self.cursor.execute(sql, args)
             print(f"执行语句:{sql} 成功")
-            return self.Cursor.fetchall()
+            return self.cursor.fetchall()
         except Exception as e:
             raise "查询有误"
         finally:
-            self.Cursor.close()
+            self.cursor.close()
 
-
-    def exe_commit(self, sql):
+    def exe_insert(self, sql, *args):
         try:
-            self.Cursor.execute(sql)
-            self.Conn.commit()
+            self.cursor.execute(sql, args)
+            self.conn.commit()
             print(f"执行语句:{sql} 成功")
         except Exception as e:
+            self.conn.rollback()
             raise f"语句:{sql}执行有误:{e}"
         finally:
-            self.Cursor.close()
+            self.conn.close()
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -42,6 +59,22 @@ age int not null
     sql3 = """
     select * from user where name='' or 1=1  #and age=29
     """
-    ins = SqliteOpera()
-    # ins.exe_commit(sql2)
-    print(ins.exe_query(sql3))
+    create_sql = """
+    create table if not exists 'public_flow'
+    ('id' integer primary key AUTOINCREMENT, 
+    'caseName' varchar(100) not null,
+    'case_desc' varchar(100) default null,
+    'request' varchar(2000) not null,
+    'reponse' varchar(2000) not null,
+    'result' varchar(4),
+    'create_time' date timestamp not null
+    DEFAULT(datetime('now', 'localtime'))
+    )
+    """
+
+    sql4 = """
+     insert into public_flow(“caseName","case_desc“,"request"，“reponse“,“result")values(“中台信用卡授权交易历史信息查询“，
+    """
+
+
+
