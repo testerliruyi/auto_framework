d86f8c8c0ff7f96c4a7d25f408bf3548c4d96931
liruyi <liruyi0229@outlook.com>
2024-01-27 06:10:25 +0000

    update common/send_request.py.
    Signed-off-by: liruyi <liruyi0229@outlook.com>

--- common/send_request.py
+++ common/send_request.py
@@ -2,13 +2,13 @@
 func: 公共请求方法，用于发起接口请求。
 """
 
-import requests,json
+import requests
 
 
 class SendRequest:
 
     @staticmethod
-    def request(url, method: str, headers, **kwargs):
+    def request(url, method: str, **kwargs):
         """
         :param headers:  接口请求头
         :param url: 请求地址
@@ -17,22 +17,29 @@ class SendRequest:
         :return:  响应结果
         """
         if method.upper() == "GET":
-            return requests.get(url, headers, **kwargs)
+            return requests.get(url, **kwargs)
         if method.upper() == "POST":
-            return requests.post(url, headers, **kwargs)
+            return requests.post(url, **kwargs)
         if method.upper() == "PUT":
-            return requests.put(url, headers, **kwargs)
+            return requests.put(url, **kwargs)
         if method.upper() == "DELETE":
             return requests.delete(url, **kwargs)
 
     @staticmethod
     def do_request(url, method, **kwargs):
-        if kwargs.get("params"):
-            headers = {"application/x-www-form-urlencoded"}
-        else:
-            headers = {"Content-Type": "application/json"}
+        """
+        :param url:
+        :param method:
+        :param kwargs:  Optional arguments that ``request`` takes
+                        json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
+                        data:(optional) Dictionary, list of tuples, bytes, or file-like
+        :return:
+        """
+        if not kwargs.get("headers"):
+            kwargs["headers"] = {"Content-Type": "application/json"}
+        return SendRequest.request(url=url, method=method, **kwargs)
+        
 
-        return SendRequest.request(url=url, method=method, headers=headers, **kwargs)
 
 
 
