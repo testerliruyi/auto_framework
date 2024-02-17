5fc30094ed23a02be8b4c44e02feee87e8b6e4e4
liruyi0229@outlook.com <liruyi0229@outlook.com>
2024-02-17 11:03:15 +0800

    格式修改

--- common/template_init/special_template_init.py
+++ common/template_init/special_template_init.py
@@ -5,33 +5,34 @@ func: 用于yaml案例中，对指定依赖案例的参数进行替换。
 import re
 from common.json_find import json_get_value
 
-def special_template_init(pattern, content:str, respone:dict):
+
+def special_template_init(pattern, content: str, response: dict):
     """
     :param pattern:  正则表达式匹配规则
     :param content: 需要处理的json报文体，字符串格式。
-    :param respone: 用于替换content中符合pattern的字段内容。
+    :param response: 用于替换content中符合pattern的字段内容。
     :return: 参数后处理后的报文内容
     """
     patt = re.compile(pattern)
-    #按素正则素达式匹配的字段
-    template_key=re.findall(patt,content)
+    # 按正则素达式匹配的字段
+    template_key = re.findall(patt, content)
     if template_key:
         if pattern == r'\${DepRep\((\w+)\)}':
             for key in template_key:
-                if json_get_value(respone,key):
+                if json_get_value(response, key):
                     try:
-                        content=content.replace("${DepRep(%s)}" % key,
-                                                str(json_get_value(respone,key)))
+                        content = content.replace("${DepRep(%s)}" % key,
+                                                  str(json_get_value(response, key)))
                     except Exception as err:
                         raise KeyError(f'{key}未拽索到，请排查下')
                 else:
                     pass
         elif pattern == r'\${DepReq\((\w+)\)}':
             for key in template_key:
-                if json_get_value(respone, key):
+                if json_get_value(response, key):
                     try:
                         content = content.replace("${DepReq(%s)}" % key,
-                                                  str(json_get_value(respone,key)))
+                                                  str(json_get_value(response, key)))
                     except Exception as err:
                         raise KeyError(f'{key}未拽索到，请排查下')
                 else:
