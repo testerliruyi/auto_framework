a0f91ff39f81cb7c0e7cd942fa3fcedb3009931c
liruyi0229@outlook.com <liruyi0229@outlook.com>
2024-02-18 15:45:56 +0800

    修复bug

--- test_manage/web_test_case/test_new_tx_channel.py
+++ test_manage/web_test_case/test_new_tx_channel.py
@@ -8,9 +8,9 @@ page = UploadVideoPage()
 @pytest.mark.parametrize("info", content_handle())
 class TestTxUploadVideo:
 
-    def setup_class(self):
-        # 打开网址
-        page.open_url()
+    # def setup_class(self):
+    #     # 打开网址
+    #     page.open_url()
 
     # 点击发布视频按钮
     def test_upload_video(self, info):
